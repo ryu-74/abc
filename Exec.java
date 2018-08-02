e6febb193da5932dc47ebfcd73171674106b9078
ryu-8 <nisifumi74@gmail.com>
2018-08-02 12:27:13 +0900

    abc

---
+++ Exec.java
@@ -0,0 +1,9 @@
+package exsample01;
+public class Exec{
+	public static void main(String[]args){
+		System.out.println("挨拶:おはよう");
+		System.out.println("挨拶:こんにちは");
+		System.out.println("挨拶:abc");
+	}
+}
+		
